6470ffdd2ad1596f196487773028cb77b398edde
Коротких Павел <vylout2016@yandex.ru>
2023-10-19 00:28:04 +0500

    first commit

---
+++ src/test/java/com/example/dango/DangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
